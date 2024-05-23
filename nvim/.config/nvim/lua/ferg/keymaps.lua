a5e66d895713249b8e7cd43c5a8c718687ae20a2
Fergus Hewson <fergus.a.hewson@outlook.com>
2024-05-23 15:34:49 +1200

    Adding error shortcuts.

--- nvim/.config/nvim/lua/ferg/keymaps.lua
+++ nvim/.config/nvim/lua/ferg/keymaps.lua
@@ -5,9 +5,12 @@ local keymap = vim.keymap
 
 keymap.set("n", "<leader>ee", vim.cmd.Ex)
 keymap.set("n", "<leader>p", "*p")
-keymap.set({"n", "v"}, "<leader>y", "*y")
+keymap.set({ "n", "v" }, "<leader>y", "*y")
 
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+
+vim.keymap.set("n", "]e", vim.diagnostic.goto_next)
+vim.keymap.set("n", "[e", vim.diagnostic.goto_prev)
