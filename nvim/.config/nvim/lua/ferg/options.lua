92da8565b2ef48cc80dc4a27f434e62a73a7519d
fergdev <fergus.a.hewson@outlook.com>
2024-05-24 14:01:27 +1200

    Update options.lua

--- nvim/.config/nvim/lua/ferg/options.lua
+++ nvim/.config/nvim/lua/ferg/options.lua
@@ -1,6 +1,17 @@
 local opt = vim.opt
-opt.guicursor = ""
+opt.clipboard = "unnamedplus"
 opt.cursorline = true
+opt.expandtab = true
+opt.guicursor = ""
+opt.ignorecase = true
+opt.incsearch = true
+opt.scrolloff = 10
+opt.shiftwidth = 4
+opt.showmode = false
+opt.smartcase = true
+opt.smarttab = true
+opt.splitbelow = true
+opt.splitright = true
 opt.tabstop = 4
-
-vim.opt.termguicolors = true
+opt.termguicolors = true
+opt.undofile = true
