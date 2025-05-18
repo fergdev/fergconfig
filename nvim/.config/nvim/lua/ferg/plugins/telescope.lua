ee7af848dab9f9cf0f41acd62f2d93e65ddc1ec6
fergdev <fergus.a.hewson@outlook.com>
2025-05-19 08:54:05 +1200

    Tightening the telescope.

--- nvim/.config/nvim/lua/ferg/plugins/telescope.lua
+++ nvim/.config/nvim/lua/ferg/plugins/telescope.lua
@@ -86,18 +86,41 @@ return {
 		pcall(require("telescope").load_extension, "ui-select")
 
 		local builtin = require("telescope.builtin")
+
+		local find_files = function()
+			builtin.find_files({
+				find_command = { "rg", "--files", "--iglob", "!.git", "--hidden" },
+				previewer = false,
+			})
+		end
+
+		local grep_string = function()
+			builtin.grep_string({
+				additional_args = function()
+					return { "--hidden", "--glob", "!.git/*" }
+				end,
+				previewer = false,
+			})
+		end
+
+		local live_grep = function()
+			builtin.live_grep({
+				additional_args = function()
+					return { "--hidden", "--glob", "!.git/*" }
+				end,
+			})
+		end
+
 		vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "[F]ind [H]elp" })
 		vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "[F]ind [K]eymaps" })
-		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "[F]ind [F]iles" })
+		vim.keymap.set("n", "<leader>ff", find_files, { desc = "[F]ind [F]iles" })
 		vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "[F]ind existing [B]uffers" })
 		vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "[F]ind [D]iagnostics" })
-
-		vim.keymap.set("n", "<leader>fs", builtin.builtin, { desc = "[F]ind [S]elect Telescope" })
-		vim.keymap.set("n", "<leader>fw", builtin.grep_string, { desc = "[F]ind current [W]ord" })
-		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "[F]ind by [G]rep" })
+		vim.keymap.set("n", "<leader>ft", builtin.builtin, { desc = "[F]ind [T]elescope" })
+		vim.keymap.set("n", "<leader>fw", grep_string, { desc = "[F]ind current [W]ord" })
+		vim.keymap.set("n", "<leader>fg", live_grep, { desc = "[F]ind by [G]rep" })
 		vim.keymap.set("n", "<leader>fr", builtin.resume, { desc = "[F]ind [R]esume" })
-		vim.keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = '[F]ind Recent Files ("." for repeat)' })
-
+		vim.keymap.set("n", "<leader>fo", builtin.oldfiles, { desc = '[F]ind Recent Files ("." for repeat)' })
 
 		vim.keymap.set("n", "<leader>f/", function()
 			builtin.live_grep({
