b3f6fa0a38f8c11ded3401f934c9a18c8c654369
fergdev <fergus.a.hewson@outlook.com>
2025-05-18 14:58:25 +1200

    Updating telescope config. Changing format key to be <leader>rl like idea.

--- nvim/.config/nvim/lua/ferg/plugins/conform.lua
+++ nvim/.config/nvim/lua/ferg/plugins/conform.lua
@@ -3,7 +3,7 @@ return {
 	lazy = false,
 	keys = {
 		{
-			"<leader>f",
+			"<leader>rl",
 			function()
 				require("conform").format({ async = true, lsp_fallback = true })
 			end,
--- nvim/.config/nvim/lua/ferg/plugins/telescope.lua
+++ nvim/.config/nvim/lua/ferg/plugins/telescope.lua
@@ -28,10 +28,10 @@ return {
 					["<C-n>"] = actions.cycle_history_next,
 					["<C-p>"] = actions.cycle_history_prev,
 
-					["<C-j>"] = actions.move_selection_next,
-					["<C-k>"] = actions.move_selection_previous,
+					-- ["<C-j>"] = actions.move_selection_next,
+					-- ["<C-k>"] = actions.move_selection_previous,
 
-					["<C-c>"] = actions.close,
+					-- ["<C-[>"] = actions.close,
 
 					["<CR>"] = actions.select_default,
 					["<C-x>"] = actions.select_horizontal,
@@ -50,8 +50,10 @@ return {
 				},
 
 				n = {
-					["<esc>"] = actions.close,
+					["<C-[>"] = actions.close,
+
 					["<CR>"] = actions.select_default,
+
 					["<C-x>"] = actions.select_horizontal,
 					["<C-v>"] = actions.select_vertical,
 					["<C-t>"] = actions.select_tab,
@@ -84,17 +86,24 @@ return {
 		pcall(require("telescope").load_extension, "ui-select")
 
 		local builtin = require("telescope.builtin")
-		vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "[S]earch [H]elp" })
-		vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
-		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "[S]earch [F]iles" })
+		vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "[F]ind [H]elp" })
+		vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "[F]ind [K]eymaps" })
+		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "[F]ind [F]iles" })
+		vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "[F]ind existing [B]uffers" })
+		vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "[F]ind [D]iagnostics" })
 
-		vim.keymap.set("n", "<leader>fs", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
-		vim.keymap.set("n", "<leader>fw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
-		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
-		vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
-		vim.keymap.set("n", "<leader>fr", builtin.resume, { desc = "[S]earch [R]esume" })
-		vim.keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-		vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
+		vim.keymap.set("n", "<leader>fs", builtin.builtin, { desc = "[F]ind [S]elect Telescope" })
+		vim.keymap.set("n", "<leader>fw", builtin.grep_string, { desc = "[F]ind current [W]ord" })
+		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "[F]ind by [G]rep" })
+		vim.keymap.set("n", "<leader>fr", builtin.resume, { desc = "[F]ind [R]esume" })
+		vim.keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = '[F]ind Recent Files ("." for repeat)' })
+
+		vim.keymap.set("n", "<leader>f/", function()
+			builtin.live_grep({
+				grep_open_files = true,
+				prompt_title = "Live Grep in Open Files",
+			})
+		end, { desc = "[F]ind [/] in Open Files" })
 
 		vim.keymap.set("n", "<leader>/", function()
 			builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
@@ -102,14 +111,8 @@ return {
 				previewer = false,
 			}))
 		end, { desc = "[/] Fuzzily search in current buffer" })
-		vim.keymap.set("n", "<leader>t/", function()
-			builtin.live_grep({
-				grep_open_files = true,
-				prompt_title = "Live Grep in Open Files",
-			})
-		end, { desc = "[S]earch [/] in Open Files" })
-		vim.keymap.set("n", "<leader>sn", function()
+		vim.keymap.set("n", "<leader>fn", function()
 			builtin.find_files({ cwd = vim.fn.stdpath("config") })
-		end, { desc = "[S]earch [N]eovim files" })
+		end, { desc = "[F]ind [N]eovim files" })
 	end,
 }
