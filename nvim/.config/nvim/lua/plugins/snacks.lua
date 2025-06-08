5ddd57110d75826abf5b36e944b245279f8292d8
Fergus Hewson <fergus_hewson@bnz.co.nz>
2025-06-08 12:29:09 +1200

    Bump

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -3,7 +3,14 @@ return {
   opts = {
     scroll = { enabled = false },
     dashboard = { enabled = false },
-    explorer = { enabled = false },
+    explorer = { enabled = true },
     indent = { animate = { enabled = false } },
+    picker = {
+      sources = {
+        explorer = {
+          hidden = true,
+        },
+      },
+    },
   },
 }
