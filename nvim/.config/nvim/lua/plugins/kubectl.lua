508c2f777b25d0ba4c9931ce186ef49ca592fe0c
fergdev <fergus.a.hewson@outlook.com>
2025-06-09 19:45:54 +1200

    Fixing kubectl.nvim

--- nvim/.config/nvim/lua/plugins/kubectl.lua
+++ nvim/.config/nvim/lua/plugins/kubectl.lua
@@ -1,5 +1,10 @@
 return {
   {
     "ramilito/kubectl.nvim",
+    -- this needs to be called so the keys work
+    config = function()
+      ---@diagnostic disable-next-line: missing-fields
+      require("kubectl").setup({})
+    end,
   },
 }
