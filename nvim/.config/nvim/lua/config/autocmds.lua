8a0e58e3486c8608d5b208764ae6510640624838
fergdev <fergus.a.hewson@outlook.com>
2025-06-14 21:36:52 +1200

    blah

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -7,23 +7,6 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
   command = "set filetype=bash",
 })
 
--- vim.schedule(function()
---   vim.opt.clipboard:append("unnamedplus")
---
---   vim.g.clipboard = {
---     name = "OSC 52",
---     copy = {
---       ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
---       ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
---     },
---     paste = {
---       ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
---       ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
---     },
---   }
--- end)
---
-
 -- close some filetypes with <q>
 
 vim.api.nvim_del_augroup_by_name("lazyvim_close_with_q")
