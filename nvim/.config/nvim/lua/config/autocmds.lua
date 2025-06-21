9fccbc8c88029f3b6a93cba2cd97724426ea9941
fergdev <fergus.a.hewson@outlook.com>
2025-06-21 12:20:52 +1200

    Adding snipe and colorizer

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -6,8 +6,9 @@ local function augroup(name)
   return vim.api.nvim_create_augroup("ferg_" .. name, { clear = true })
 end
 
-vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-  pattern = "*.zsh*",
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+  group = augroup("bash_ft"),
+  pattern = { "*.zsh*", "sketchybarrc" },
   command = "set filetype=bash",
 })
 
